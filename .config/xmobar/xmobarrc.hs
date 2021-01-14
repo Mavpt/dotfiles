114fad16df0a8a3a8d63347d7c309b9f8621b8c9
Bruno <bm.contas.oliveira@gmail.com>
2021-01-14 00:39:12 +0000

    Added xmonad and xmobar configurations. Still trying to figure out DBUS.

---
+++ .config/xmobar/xmobarrc.hs
@@ -0,0 +1,97 @@
+Config { 
+
+   -- appearance
+     font =         "xft:MESLO LGS NF:pixelsize=15"
+
+   , bgColor =      "#2E3440"
+   , fgColor =      "#ECEFF4"
+
+   , position =     Static { xpos = 0 , ypos = 0, width = 1920, height = 32 }
+
+   , border =       BottomB
+   , borderColor =  "#5E81AC"
+
+   -- layout
+   , sepChar =  "%"   -- delineator between plugin names and straight text
+   , alignSep = "}{"  -- separator between left-right alignment
+   , template = "%StdinReader% %battery% | %multicpu% | %coretemp% | %memory% | %dynnetwork% }{ %mpd% | %RJTT% | %date% || %kbd% "
+
+   -- general behavior
+   , lowerOnStart =     True    -- send to bottom of window stack on start
+   , hideOnStart =      False   -- start with window unmapped (hidden)
+   , allDesktops =      True    -- show on all desktops
+   , overrideRedirect = True    -- set the Override Redirect flag (Xlib)
+   , pickBroadest =     False   -- choose widest display (multi-monitor)
+   , persistent =       True    -- enable/disable hiding (True = disabled)
+
+   -- plugins
+   , commands = 
+        -- weather monitor
+        [ Run Weather "RJTT" [ "--template", "<skyCondition> | <fc=#4682B4><tempC></fc>°C | <fc=#4682B4><rh></fc>% | <fc=#4682B4><pressure></fc>hPa"
+                             ] 36000
+
+        -- network activity monitor (dynamic interface resolution)
+        , Run DynNetwork     [ "--template" , "<dev>: <tx>kB/s|<rx>kB/s"
+                             , "--Low"      , "1000"       -- units: B/s
+                             , "--High"     , "5000"       -- units: B/s
+                             , "--low"      , "#A3BE8C"
+                             , "--normal"   , "#D08770"
+                             , "--high"     , "#BF616A"
+                             ] 10
+
+        -- cpu activity monitor
+        , Run MultiCpu       [ "--template" , "Cpu: <total0>%|<total1>%"
+                             , "--Low"      , "50"         -- units: %
+                             , "--High"     , "85"         -- units: %
+                             , "--low"      , "#A3BE8C"
+                             , "--normal"   , "#D08770"
+                             , "--high"     , "#BF616A"
+                             ] 10
+
+        -- cpu core temperature monitor
+        , Run CoreTemp       [ "--template" , "Temp: <core0>°C|<core1>°C"
+                             , "--Low"      , "70"        -- units: °C
+                             , "--High"     , "80"        -- units: °C
+                             , "--low"      , "#A3BE8C"
+                             , "--normal"   , "#D08770"
+                             , "--high"     , "#BF616A"
+                             ] 50
+                          
+        -- memory usage monitor
+        , Run Memory         [ "--template" ,"Mem: <usedratio>%"
+                             , "--Low"      , "20"        -- units: %
+                             , "--High"     , "90"        -- units: %
+                             , "--low"      , "#A3BE8C"
+                             , "--normal"   , "#D08770"
+                             , "--high"     , "#BF616A"
+                             ] 10
+
+        -- battery monitor
+        , Run Battery        [ "--template" , "Batt: <acstatus>"
+                             , "--Low"      , "10"        -- units: %
+                             , "--High"     , "80"        -- units: %
+                             , "--low"      , "#BF616A"
+                             , "--normal"   , "#D08770"
+                             , "--high"     , "#A3BE8C"
+
+                             , "--" -- battery specific options
+                                       -- discharging status
+                                       , "-o"	, "<left>% (<timeleft>)"
+                                       -- AC "on" status
+                                       , "-O"	, "<fc=#dAA520>Charging</fc>"
+                                       -- charged status
+                                       , "-i"	, "<fc=#006000>Charged</fc>"
+                             ] 50
+
+        -- time and date indicator 
+        --   (%F = y-m-d date, %a = day of week, %T = h:m:s time)
+        , Run Date           "<fc=#ABABAB>%F (%a) %T</fc>" "date" 10
+
+        -- keyboard layout indicator
+        , Run Kbd            [ ("us(dvorak)" , "<fc=#00008B>DV</fc>")
+                             , ("us"         , "<fc=#8B0000>US</fc>")
+                             ]
+
+        -- MPD
+        ]
+   } 
